19ce4c7d7078352b59810d98bc82961bfa02424e
Martin Růžek <me@martinruzek.cz>
2025-06-21 11:22:21 +0200

    fixed pointer on some components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
 import { Button } from "@/components/ui/button";
+import { api } from "../../convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Link } from "react-router";
 
 export function Header({
   isLoading,
@@ -9,14 +12,24 @@ export function Header({
   isAuthenticated: boolean;
   onLogout: () => void;
 }) {
+  const isAdmin = useQuery(api.users.isAdmin);
   return (
     <header className="flex justify-between items-center mb-8">
       <h1 className="text-3xl font-bold">Včelí vzpomínky pro Adama 🐝</h1>
-      {!isLoading && isAuthenticated && (
-        <Button onClick={onLogout} className="cursor-pointer">
-          Odhlásit se
-        </Button>
-      )}
+      <nav className="flex space-x-4">
+        {!isLoading && isAuthenticated && isAdmin && (
+          <Button>
+            <Link className="cursor-pointer" to="/memories">
+              Správa vzpomínek
+            </Link>
+          </Button>
+        )}
+        {!isLoading && isAuthenticated && (
+          <Button onClick={onLogout} className="cursor-pointer">
+            Odhlásit se
+          </Button>
+        )}
+      </nav>
     </header>
   );
 }
